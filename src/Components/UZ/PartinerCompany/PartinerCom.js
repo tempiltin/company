e11db25429e4bc5c285cfc1af8b3e1df6a61c66e
__________ <temurbekshukurov0707@gmail.com>
2022-05-19 12:59:33 +0500

    upload

--- src/Components/UZ/PartinerCompany/PartinerCom.js
+++ src/Components/UZ/PartinerCompany/PartinerCom.js
@@ -1,11 +1,76 @@
 import React from 'react'
-
+import img from '../../../assets/payme_03.png'
 const PartinerCom = () => {
-  return (
-    <section>
-        
-    </section>
-  )
+    const data = [
+        {
+            img: img,
+            aos: "asd"
+        },
+        {
+            img: img,
+            aos: "asd"
+        },
+        {
+            img: img,
+            aos: "asd"
+        },
+        {
+            img: img,
+            aos: "asd"
+        },
+        {
+            img: img,
+            aos: "asd"
+        },
+        {
+            img: img,
+            aos: "asd"
+        },
+        {
+            img: img,
+            aos: "asd"
+        },
+        {
+            img: img,
+            aos: "asd"
+        },
+        {
+            img: img,
+            aos: "asd"
+        },
+        {
+            img: img,
+            aos: "asd"
+        },
+        {
+            img: img,
+            aos: "asd"
+        },
+        {
+            img: img,
+            aos: "asd"
+        },
+      
+    ]
+    return (
+        <section className='parentCompany'>
+            <div className="container">
+                <h6>Our Awesome Customer Relation Build a
+                    Long Term Relationship.</h6>
+                <div className="row mt-5 ">
+                 {
+                     data.map(({img},index) => (
+                        <div className="col-6 col-sm-3 col-md-2 col-lg-2" key={index}>
+                        <div className="briend">
+                         <img src={img} alt="" />
+                        </div>
+                      </div>
+                     ))
+                 }
+                </div>
+            </div>
+        </section>
+    )
 }
 
 export default PartinerCom
